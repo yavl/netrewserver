16582ac3e90e2b3f78f73c18c4d7b814ef5e48da
Vlad <yavl98@mail.ru>
2020-04-20 04:33:57 +0900

    upd

--- src/com/netrewserver/GameServer.kt
+++ src/com/netrewserver/GameServer.kt
@@ -8,6 +8,12 @@ import com.netrewserver.requests.SomeRequest
 class GameServer : Listener() {
     val server = Server()
 
+    init {
+        server.kryo.register(SomeRequest::class.java)
+        server.start()
+        server.bind(13370, 13371)
+    }
+
     override fun connected(connection: Connection) {
         val clientIP = connection.remoteAddressTCP.hostString
         println("${clientIP} connected")
